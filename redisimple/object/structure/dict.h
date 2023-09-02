035326c0f9fdad717ce656013e84d38dbc576098
mirthfulLee <lihuan0530@gmail.com>
2023-09-02 14:55:46 +0800

    src forder renamed

---
+++ redisimple/object/structure/dict.h
@@ -0,0 +1,7 @@
+#ifndef REDISIMPLE_OBJECT_STRUCTURE_DICT_H_
+#define REDISIMPLE_OBJECT_STRUCTURE_DICT_H_
+#include <memory>
+namespace redisimple::object::structure {
+
+}
+#endif // REDISIMPLE_OBJECT_STRUCTURE_DICT_H_
