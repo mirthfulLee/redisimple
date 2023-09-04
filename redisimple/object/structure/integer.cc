0c07d193b20cd8524fb0841850caf522d103e12c
mirthfulLee <lihuan0530@gmail.com>
2023-09-04 18:11:27 +0800

    add integer

---
+++ redisimple/object/structure/integer.cc
@@ -0,0 +1,29 @@
+#include "integer.h"
+
+#include <algorithm>
+
+#include "redisimple/config.h"
+#include "redisimple/util/hash.h"
+#include "redisimple_data_structure.h"
+
+using redisimple::Config;
+namespace redisimple::object::structure {
+int Integer::compare(RedisimpleDataStructure* value) {
+  Integer* vptr = dynamic_cast<Integer*>(value);
+  if (vptr->value_ == value_)
+    return 0;
+  else
+    return vptr->value_ > value_ ? 1 : -1;
+}
+RedisimpleStructureType Integer::structure_type() {
+  return REDISIMPLE_STRUCTURE_INT;
+}
+std::unique_ptr<RedisimpleDataStructure> Integer::duplicate() {
+  return std::unique_ptr<Integer>(new Integer(value_));
+}
+int size() { return sizeof(int); }
+int Integer::hash() {
+  return redisimple::util::murmurhash2(&value_, sizeof(int),
+                                       Config::get_instance()->random_seed);
+}
+}  // namespace redisimple::object::structure
---
+++ redisimple/object/structure/integer.h
@@ -0,0 +1,20 @@
+#ifndef REDISIMPLE_OBJECT_STRUCTURE_INTEGER_H_
+#define REDISIMPLE_OBJECT_STRUCTURE_INTEGER_H_
+
+#include "redisimple_data_structure.h"
+namespace redisimple::object::structure {
+class Integer : public RedisimpleDataStructure {
+ public:
+  Integer() : value_(0) {}
+  Integer(int value) : value_(value) {}
+  int compare(RedisimpleDataStructure*);
+  RedisimpleStructureType structure_type();
+  std::unique_ptr<RedisimpleDataStructure> duplicate();
+  int size();
+  int hash();
+
+ private:
+  int value_;
+};
+}  // namespace redisimple::object::structure
+#endif  // REDISIMPLE_OBJECT_STRUCTURE_INTEGER_H_
