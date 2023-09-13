760bdcc55841f13309b1ee8da1a59737985ae2e7
mirthfulLee <lihuan0530@gmail.com>
2023-09-13 22:31:35 +0800

    add defination of server&client

--- redisimple/client.h
+++ redisimple/client.h
@@ -1,4 +1,55 @@
 #ifndef REDISIMPLE_CLIENT_H_
 #define REDISIMPLE_CLIENT_H_
-namespace redisimple {}
+#include <memory>
+
+#include "redisimple/data_base.h"
+#include "redisimple/object/list/linked_list.h"
+namespace redisimple {
+
+// Each client link to an socket that linked to socket server
+
+class Client {
+ public:
+  Client(int fd, int flag = 0);
+
+ public:
+  // read data from socket
+  // get argv & argc (tokenize in_buffer)
+  // chose handler with argv[0]
+  // execute handler
+  int execute();
+
+ private:
+  int fd_;
+  DataBase* selected_db_;
+  bool authentecated_;
+  int expire_time_;
+  int flags_;
+  std::unique_ptr<char[]> in_buffer_;
+  std::unique_ptr<char[]> out_buffer_;
+  std::unique_ptr<char*[]> argv;
+  int argc;
+  // TODO: processor
+ private:
+  int read_data();
+  int input_resolve();
+  int chose_processor();
+  int start_process();
+};
+typedef LinkedListNode<Client> ClientNode;
+class ClientList {
+ public:
+  ClientList();
+
+ public:
+  int add_client(int fd, int flag);
+  int delete_client_with_fd(int fd);
+  int delete_expired_client();
+  int get_client_with_fd(int fd);
+
+ private:
+  std::unique_ptr<ClientNode> head_;
+  int client_num_;
+};
+}  // namespace redisimple
 #endif  // REDISIMPLE_CLIENT_H_
--- redisimple/server.h
+++ redisimple/server.h
@@ -3,11 +3,29 @@
 #include <memory>
 
 #include "data_base.h"
+#include "redisimple/client.h"
 namespace redisimple {
 class Server {
  public:
+  int load_config();
+
+  int listen_init();
+  // accept new connection(client);
+  // deal with timer event;
+  // process new request of client;
+  // output result of process
+  int start_main_loop();
+
  private:
-  std::unique_ptr<DataBaseList> dbs_;
+  std::unique_ptr<DataBaseList> db_list_;
+  std::unique_ptr<ClientList> client_list_;
+  // TODO: event listener
+
+ private:
+  int new_client();
+  int timer_event();
+  int process_request(int fd);
+  int output_result(int fd);
 };
 }  // namespace redisimple
 #endif  // REDISIMPLE_SERVER_H_
